1b4c7d841db13ed05c0db63fd185643ec69dc15d
Jordan Coeyman <coeyman@gmail.com>
2025-10-23 14:31:01 -0400

    Switch adapter based on NODE_ENV

--- packages/create-remote-app/template/svelte.config.js
+++ packages/create-remote-app/template/svelte.config.js
@@ -1,14 +1,16 @@
 import alchemy from 'alchemy/cloudflare/sveltekit';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: { 
-		adapter: alchemy(),
+		adapter: dev ? alchemy() : adapter(),
     experimental: {
 			remoteFunctions: true
-		}
+		} 
   },
   compilerOptions: {
 		experimental: {
